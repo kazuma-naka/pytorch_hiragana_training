7c4225b8d2371efd383afa74f9e1b8d232ad7ba5
KazumaProject <59742125+KazumaProject@users.noreply.github.com>
2026-02-01 13:58:04 -0500

    add numbers for labels

--- train_hiragana_crnn_ctc.py
+++ train_hiragana_crnn_ctc.py
@@ -45,7 +45,22 @@ def hiragana_charset(
     for cp in range(0x3041, 0x3097):  # ぁ..ゖ
         chars.append(chr(cp))
     if include_punct:
-        chars += ["ー", "、", "。", "ペ"]
+        chars += [
+            "ー",
+            "、",
+            "。",
+            "ペ",
+            "0",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+        ]
     if include_space:
         chars += [" "]
     out: List[str] = []
